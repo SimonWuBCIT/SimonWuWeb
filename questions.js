e878e788b32dcaf0097e53e4fd5ec295902075c3
SimonWuBCIT <swu111@my.bcit.ca>
2019-02-13 18:18:00 -0800

    test

--- questions.js
+++ questions.js
@@ -51,34 +51,34 @@ function populateQuestions() {
 function populateOptions(question_number) {
     switch (question_number) {
         case 0:
-            options[0] = "<input type='radio' name='question1' value='0'>The ''owner'' of the method<br>"
-            options[1] = "<input type='radio' name='question1' value='1'>The global object<br>"
-            options[2] = "<input type='radio' name='question1' value='2'>The immediate child of the ''owner''<br>"
-            options[3] = "<input type='radio' name='question1' value='3'>Invalid<br><br>"
+            options[0] = "<input type=''radio'' name=''question1'' value=''0''>The ''owner'' of the method<br>"
+            options[1] = "<input type=''radio'' name=''question1'' value=''1''>The global object<br>"
+            options[2] = "<input type=''radio'' name=''question1'' value=''2''>The immediate child of the ''owner''<br>"
+            options[3] = "<input type=''radio'' name=''question1'' value=''3''>Invalid<br><br>"
             break;
         case 1:
-            options[1] = "<input type='radio' name='question2' value='0'>The global object<br>"
-            options[2] = "<input type='radio' name='question2' value='1'>Invalid<br>"
-            options[3] = "<input type='radio' name='question2' value='2'>The HTML element that received the event<br>"
-            options[4] = "<input type='radio' name='question2' value='3'>The parent of the HTML element<br><br>"
+            options[1] = "<input type=''radio'' name=''question2'' value=''0''>The global object<br>"
+            options[2] = "<input type=''radio'' name=''question2'' value=''1''>Invalid<br>"
+            options[3] = "<input type=''radio'' name=''question2'' value=''2''>The HTML element that received the event<br>"
+            options[4] = "<input type=''radio'' name=''question2'' value=''3''>The parent of the HTML element<br><br>"
             break;
         case 2:
-            options[0] = "<input type='radio' name='question3' value='0'>The owner of the function<br>"
-            options[1] = "<input type='radio' name='question3' value='1'>The function pointer<br>"
-            options[2] = "<input type='radio' name='question3' value='2'>The return value<br>"
-            options[3] = "<input type='radio' name='question3' value='3'>None of the above<br><br>"
+            options[0] = "<input type=''radio'' name=''question3'' value=''0''>The owner of the function<br>"
+            options[1] = "<input type=''radio'' name=''question3'' value=''1''>The function pointer<br>"
+            options[2] = "<input type=''radio'' name=''question3'' value=''2''>The return value<br>"
+            options[3] = "<input type=''radio'' name=''question3'' value=''3''>None of the above<br><br>"
             break;
         case 3:
-            options[0] = "<input type='radio' name='question4' value='0'>undefined<br>"
-            options[1] = "<input type='radio' name='question4' value='1'>null<br>"
-            options[2] = "<input type='radio' name='question4' value='2'>x<br>"
-            options[3] = "<input type='radio' name='question4' value='3'>The global object<br><br>"
+            options[0] = "<input type=''radio'' name=''question4'' value=''0''>undefined<br>"
+            options[1] = "<input type=''radio'' name=''question4'' value=''1''>null<br>"
+            options[2] = "<input type=''radio'' name=''question4'' value=''2''>x<br>"
+            options[3] = "<input type=''radio'' name=''question4'' value=''3''>The global object<br><br>"
             break;
         default:
-            options[0] = "<input type='radio' name='question5' value='0'>null<br>"
-            options[1] = "<input type='radio' name='question5' value='1'>Undefined<br>"
-            options[2] = "<input type='radio' name='question5' value='2'>person2<br>"
-            options[3] = "<input type='radio' name='question5' value='3'>0<br><br>"
+            options[0] = "<input type=''radio'' name=''question5'' value=''0''>null<br>"
+            options[1] = "<input type=''radio'' name=''question5'' value=''1''>Undefined<br>"
+            options[2] = "<input type=''radio'' name=''question5'' value=''2''>person2<br>"
+            options[3] = "<input type=''radio'' name=''question5'' value=''3''>0<br><br>"
             break;
     }
 }
