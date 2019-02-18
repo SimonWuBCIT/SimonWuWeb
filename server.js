807bc2fab975147590dfaedebb05a2d08043902a
SimonWuBCIT <swu111@my.bcit.ca>
2019-02-17 19:21:40 -0800

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-const database = require('./public/COMP4711/MemoryGame/JavaScript/database');
+let database = require('./public/COMP4711/MemoryGame/JavaScript/database');
 
 app.use(express.static(path.join(__dirname, "public")));
 
