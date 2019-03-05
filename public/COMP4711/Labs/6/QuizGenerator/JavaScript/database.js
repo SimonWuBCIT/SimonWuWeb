74718a340c5f35e057a26894b497d8085e59bc3f
SimonWuBCIT <swu111@my.bcit.ca>
2019-03-05 14:11:31 -0800

    corrected order in dropping table

--- public/COMP4711/Labs/6/QuizGenerator/JavaScript/database.js
+++ public/COMP4711/Labs/6/QuizGenerator/JavaScript/database.js
@@ -13,13 +13,12 @@ exports.setUpDatabase = async function () {
             if (err) throw err;
             console.log("Connected!");
 
-            await dropTable();
-
             connect.query("CREATE DATABASE IF NOT EXISTS quizdb", async function (err, result) {
                 if (err) throw err;
                 console.log("Database created");
                 closeConnection(connect);
                 await connectDatabase();
+                await dropTable();
                 await createTable();
                 await closeConnection(con);
                 resolve();
